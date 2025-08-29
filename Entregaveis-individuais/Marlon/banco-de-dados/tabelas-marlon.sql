240df1e4759a9ce22c42c72575360b577ba630fd
marlonzl7 <marlon.silva@sptech.school>
2025-08-29 00:44:07 -0300

    alterações no script de banco de dados individual

--- Entregaveis-individuais/Marlon/banco-de-dados/tabelas-marlon.sql
+++ Entregaveis-individuais/Marlon/banco-de-dados/tabelas-marlon.sql
@@ -1,21 +1,40 @@
-create database projeto_pi;
+create database sengas;
 
-use projeto_pi;
-
-create table role (
-	id_role int auto_increment primary key,
-    nome varchar(50) unique not null,
-    descricao varchar(255)
-);
+use sengas;
 
 create table usuario (
 	id_usuario int auto_increment primary key,
-    id_role int not null,
     nome varchar(100),
     email varchar(150) unique not null,
     senha varchar(255) not null,
+	nivel_acesso tinyint,
     
-    foreign key (id_role) references role (id_role)
+    constraint chkNivelAcesso check (nivel_acesso in (1, 2, 3))
+);
+
+create table empresa_cliente (
+	id_empresa_cliente int auto_increment primary key,
+    razao_social varchar(100),
+    cnpj char(14),
+    data_firma_contrato date,
+    status_contrato tinyint
+
+	constraint chkStatusContrato check (status_contrato in (1, 2))
+);
+
+create table filial (
+	id_filial int auto_increment primary key,
+    nome varchar(100)
+);
+
+create table endereco_filial (
+	id_endereco int auto_increment primary key,
+    cep char(8) not null,
+    rua varchar(150),
+    numero varchar(12),
+    bairro varchar(150),
+    cidade varchar(150),
+    estado char(2)
 );
 
 create table setor (
@@ -26,18 +45,28 @@ create table setor (
 
 create table sensor (
 	id_sensor int auto_increment primary key,
-    id_setor int not null,
-    nome varchar(100) not null,
-    status boolean,
-    
-    foreign key (id_setor) references setor (id_setor)
+    nome varchar(100)
+);
+
+create table registro_monitoramento (
+	id_registro_monitoramento int auto_increment primary key,
+	valor decimal(5, 2),
+    data_hora_registro datetime default current_timestamp
 );
 
 create table registro_vazamento (
 	id_registro_vazamento int auto_increment primary key,
-    id_sensor int not null,
     data_hora_inicio datetime default current_timestamp,
-    data_hora_fim datetime,
-
-    foreign key (id_sensor) references sensor (id_sensor)
+    data_hora_fim datetime
 );
+
+-- INSERINDO DADOS
+insert into usuario (nome, email, senha, nivel_acesso) values 
+	('Marlon de Souza', 'marlon.souza@gmail.com', 'senha123', 1),
+	('Mateus Queiroz', 'mateus.queiroz@gmail.com', 'senha321', 2),
+	('Victor Lima', 'victor.lima@gmail.com', 'senha324', 2),
+	('Vinicius Silva', 'vinicius.silva@gmail.com', 'senha281', 2),
+	('Ricardo Perdigão', 'ricardo.perdigao@gmail.com', 'senha129', 3),
+	('Lucas Eiki', 'lucas.eiki@gmail.com', 'senha291', 3);
+    
+-- Inserir mais dados e fazer selects depois
