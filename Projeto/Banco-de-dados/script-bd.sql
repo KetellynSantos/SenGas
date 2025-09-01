cef20189745783349bba0ec4593872a739b82f49
VictorSdePasta <victor.delima@sptech.school>
2025-09-01 15:57:49 -0300

    finalização do script de banco de dados

--- Projeto/Banco-de-dados/script-bd.sql
+++ Projeto/Banco-de-dados/script-bd.sql
@@ -1,9 +1,11 @@
+DROP DATABASE sengas;
 CREATE DATABASE sengas;
 USE sengas;
 
 CREATE TABLE empresa (
   idEmpresa int primary key auto_increment,
   razaoSocial varchar(80),
+  nomeFantasia varchar(80),
   cnpj char(14),
   dtFirma date,
   contratoAtivo tinyint
@@ -17,12 +19,12 @@ CREATE TABLE usuario (
   telefone char(11),
   senha varchar(255),
   nivelAcesso tinyint,
-  constraint chkNivel check(nivelAcesso >=1 and nivelAcesso <=3)
+  constraint chkNivel check(nivelAcesso >=1 and nivelAcesso <=5)
 );
 
 CREATE TABLE endereco (
   idEndereco int primary key auto_increment,
-  rua varchar(130),
+  logradouro varchar(130),
   numero int,
   complemento varchar(130),
   cidade varchar(100),
@@ -51,3 +53,30 @@ CREATE TABLE captura (
   valor decimal(5,2),
   dtRegistro datetime default current_timestamp
 );
+
+INSERT INTO empresa (razaoSocial, nomeFantasia, cnpj, contratoAtivo) VALUES
+	('Vazamento gasoso LTDA','VazGas', '01001001000101', 1),
+	('Gas explosivo SS LTDA', 'XPLOD', '02002002000202', 0),
+	('BioGrass Metano LTDA', 'BioGrass', '03003003000303', 1);
+INSERT INTO setor (titulo) VALUES
+	('Captação'),
+	('Extração'),
+	('Armazenagem'),
+	('Purificação');
+INSERT INTO endereco (logradouro, numero, cidade, uf, cep) VALUES
+	('Avenida das laranjas', '1000', 'São Paulo', 'SP', '01234001'),
+	('Avenida das ameixas', '2000', 'Minas Gerais', 'MG', '43210100'),
+	('Avenida dos limões', '3000', 'Rio de Janeiro', 'RJ', '34120010');
+INSERT INTO usuario (nome, sobrenome, telefone, nivelAcesso, email, senha) VALUES
+	('Victor', 'Domingo', '11290020002',3, 'victor@vazgaz.com.br', 'domingo'),
+    ('Igor', 'Segunda', '11190010001', 2, 'igor@vazgaz.com.br', 'segundafeira'),
+	('Bianca', 'Terça', '11190030003', 1, 'bianca@vazgaz.com.br', 'tercafeira');
+INSERT INTO sensor (titulo) VALUES
+	('Sensor fabriacaçao'),
+	('Sensor tubulação'),
+	('Sensor armazenagem');
+SELECT nomeFantasia AS 'Nome Fantasia', concat(razaoSocial, ' ', cnpj) AS 'Razão social e CNPJ', contratoAtivo AS 'Contrato Ativo' FROM empresa;
+SELECT titulo AS Setor FROM setor;
+SELECT concat(logradouro, ', ', numero, ' - ', cidade, ' - ' , uf, ' - ', cep) AS 'Endereço completo', complemento AS Complemento FROM endereco;
+SELECT concat(nome, ' ', sobrenome) AS 'Dados do funcionário (Nome completo)', concat(telefone, ' / ', email, ' / ', senha) AS 'Dados de acesso (telefne, e-mail e senha)', nivelAcesso AS 'Nivel de acesso' FROM usuario;
+SELECT idSensor AS Sensor, dtRegistro AS 'Data e horario da detecção' FROM captura;
