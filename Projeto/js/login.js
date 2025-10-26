add08254316b61d0732f43e0239aa81bad184016
ViniciusFrancelino <vinicius.fsilva@sptech.school>
2025-10-25 21:47:46 -0300

    atualização: correção do BUG repetição infinita no while

--- Projeto/js/login.js
+++ Projeto/js/login.js
@@ -6,18 +6,25 @@ function entrar() {
   while (tentativas > 0) {
     if (email == "vinicius@sengas.com.br" && pass == "q1Q!q1Q!") {
       window.location.href = 'dashboard.html';
+      break;
     } else if (email == "davi@sengas.com.br" && pass == "123@Davi") {
       window.location.href = 'dashboard.html';
+      break;
     } else if (email == "gabriela@sengas.com.br" && pass == "123@Gabriela") {
       window.location.href = 'dashboard.html';
+      break;
     } else if (email == "manuela@sengas.com.br" && pass == "123@Manuela") {
       window.location.href = 'dashboard.html';
+      break;
     } else if (email == "ketellyn@sengas.com.br" && pass == "123@Ketellyn") {
       window.location.href = 'dashboard.html';
+      break;
     } else if (email == "breno@sengas.com.br" && pass == "123@Breno") {
       window.location.href = 'dashboard.html';
+      break;
     } else if (email == "teste@sengas.com.br" && pass == "123@Teste") {
       window.location.href = 'dashboard.html';
+      break;
     } else {
       tentativas--;
       console.log(tentativas);
