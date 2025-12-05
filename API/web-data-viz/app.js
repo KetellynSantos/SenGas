4d98a36dea61b17f0873dac6afbcab5cc1f0209b
ViniciusFrancelino <vinicius.fsilva@sptech.school>
2025-12-05 18:55:32 -0300

    Ataulização: Arquivos .env e .gitignore

--- API/web-data-viz/app.js
+++ API/web-data-viz/app.js
@@ -1,9 +1,4 @@
-// var ambiente_processo = 'producao';
-var ambiente_processo = 'producao';
-
-var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
-// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
-// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+var caminho_env = '.env';
 
 require("dotenv").config({ path: caminho_env });
 
@@ -18,8 +13,6 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var adminRouter = require("./src/routes/admin");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,8 +23,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/admin", adminRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
@@ -45,7 +36,6 @@ app.listen(PORTA_APP, function () {
     \n\n\n                                                                                                 
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
-    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
