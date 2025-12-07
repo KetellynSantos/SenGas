43f7ebae59eaf006d4071a40784d684d7bd0271b
GabrielaPereiraSantana <gabriela.santana@sptech.school>
2025-12-07 12:34:14 -0300

    adiciona: funções na dashboard principal

--- API/web-data-viz/src/controllers/dashControllers.js
+++ API/web-data-viz/src/controllers/dashControllers.js
@@ -1,41 +1,50 @@
 var dashModel = require("../models/dashModel");
 
 function MedidasKpi(req, res) {
+    var idEmpresa = req.params.idEmpresa;
 
-
-    var idUsuario = req.params.idUsuario;
-
-    dashModel.MedidasKpi(idUsuario).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
+    dashModel.MedidasKpi(idEmpresa)
+        .then(resultado => {
+            if (resultado.length > 0) res.json(resultado[0]);
+            else res.status(204).send("Nenhum resultado encontrado");
+        })
+        .catch(erro => {
+            res.status(500).json(erro.sqlMessage);
+        });
 }
 
 function MedidasGrafico(req, res) {
+    var idEmpresa = req.params.idEmpresa;
 
-   var idUsuario = req.params.idUsuario;
+    dashModel.MedidasGrafico(idEmpresa)
+        .then(resultado => {
+            if (resultado.length > 0) res.json(resultado);
+            else res.status(204).send("Nenhum resultado encontrado");
+        })
+        .catch(erro => {
+            res.status(500).json(erro.sqlMessage);
+        });
+}
 
-    dashModel.MedidasGrafico(idUsuario).then(function (resultado) {
-           if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
+function obterLimite(req, res) {
+    const idEmpresa = req.params.idEmpresa;
+
+    dashModel.buscarLimite(idEmpresa)
+        .then(resultado => {
+            if (resultado.length > 0) {
+                res.json(resultado[0]); // retorna { limite: X }
+            } else {
+                res.status(404).send("Nenhum limite encontrado.");
+            }
+        })
+        .catch(erro => {
+            console.log("Erro ao buscar limite:", erro);
+            res.status(500).json(erro);
+        });
 }
 
 module.exports = {
-     MedidasKpi,
-    MedidasGrafico
-}
+    MedidasKpi,
+    MedidasGrafico,
+    obterLimite
+};
