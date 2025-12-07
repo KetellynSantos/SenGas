1f54befc71d8847905048a97b8acec02312fadb8
ViniciusFrancelino <vinicius.fsilva@sptech.school>
2025-12-07 11:36:17 -0300

    Adicionando: Adicionando tela de Historico a API, criando todas as rotas do backend, e criando o front-end que traz as informações do back-end

--- API/web-data-viz/app.js
+++ API/web-data-viz/app.js
@@ -13,6 +13,7 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var adminRouter = require("./src/routes/admin");
+var historicoRouter = require("./src/routes/historico");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,7 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/admin", adminRouter);
+app.use("/historico", historicoRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
