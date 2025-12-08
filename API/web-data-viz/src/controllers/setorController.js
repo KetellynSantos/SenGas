aaf0c59568a75427a0e7409fd25b487816958492
ManuelaPrado <manuela.garcia@sptech.school>
2025-12-07 23:37:00 -0300

    Atualização: Subindo atualização do controller

--- API/web-data-viz/src/controllers/setorController.js
+++ API/web-data-viz/src/controllers/setorController.js
@@ -2,8 +2,9 @@ var setorModel = require("../models/setorModel");
 
 function buscarPorSetor(req, res) {
     const idEmpresa = req.params.idEmpresa;
+    const setor = req.params.setor;
 
-    setorModel.buscarSetor(idEmpresa)
+    setorModel.buscarSetor(idEmpresa, setor)
         .then((resultado) => {
             res.status(200).json(resultado);
         })
